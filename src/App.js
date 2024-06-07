d3764791ce58c0b574115d4142493cc29a4f89eb
vincitfx <vincitfx@gmail.com>
2024-06-07 19:27:31 +0530

    19:27

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router basename="/mrighas_webapp">
+      <Router basename="/">
           <Routes>
             <Route exact path="/" element={<HomePage />} />
           </Routes>
